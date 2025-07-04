38627da3ef50b2f976a99b74742dce25d8b2e53f
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-07-04 17:21:38 +0900

    💬 Added quotes and lecture codes

--- my-workspace/ch10-nextjs/02-cna/src/data/functions/boardFetch.ts
+++ my-workspace/ch10-nextjs/02-cna/src/data/functions/boardFetch.ts
@@ -1,14 +1,21 @@
-'use server'; // 서버 함수
+'use server'; // 서버함수
 
-import { Post } from "@/types/board";
+import { Post } from '@/types/board';
 
 export async function fetchPosts(): Promise<Post[]> {
-  // 준비된 API 서버 호출
+  // 준비된 api서버 호출
   const res = await fetch(`https://fesp-api.koyeb.app/market/posts?type=qna`, {
     headers: {
-      'Client-Id': 'openmarket'
-    }
+      'Client-Id': 'openmarket',
+    },
+
+    next: {
+      tags: ['list', '3'],
+    },
+    // cache: "no-cache", // next15 기본값
+    cache: 'force-cache', // next14 기본값
   });
+
   const data = await res.json();
   console.log('boardFetch', data.item.length);
   return data.item;
